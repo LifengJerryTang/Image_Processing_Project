e25b70bc6b8f7330c0aab31effdffd0d1cd61106
Jerry Tang <jerrytang16@gmail.com>
2023-08-21 22:28:32 -0400

    need to fix lint errors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express from "express";
-import {promises as fs, existsSync} from "fs";
-import path from "path";
+import express from 'express';
+import { promises as fs, existsSync } from "fs";
+import path from 'path';
 import imagesRouter from './routes/images'
 
 const app = express();
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -5,18 +5,29 @@ import path from "path";
 
 const request = supertest(app);
 describe("Test image processing endpoint responses", (): void => {
+
+    const imageName = 'fjord';
+    const width = 200;
+    const height = 250;
+    const url = `/api/images?filename=${imageName}&width=${width}&height=${height}`;
+
     it('Returns successful response code when query params are valid',
         async () => {
-        const imageName = 'fjord';
-        const width = 200;
-        const height = 250;
-        const url = `/api/images?filename=${imageName}&width=${width}&height=${height}`;
 
         const response = await request.get(url);
+
+        expect(response.status).toBe(200);
+
+
+    });
+
+    it("Produces the correctly processed image when query params are valid",    async () => {
+
+        await request.get(url);
+
         const thumbFilePath =
             path.resolve(__dirname, `../assets/thumbnails/${imageName}_thumb_${width}_${height}.jpg`)
 
-        expect(response.status).toBe(200);
         expect(existsSync(thumbFilePath)).toBeTruthy();
 
     })
